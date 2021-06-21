5c435c4b5275b2c93a87e54e00f702f601e60d0a
gunn3r71 <lucasgunner3@gmail.com>
2021-06-20 22:36:09 -0300

    Adicionar arquivos de projeto.

---
+++ API.CRUD/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using API.CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.CRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ API.CRUD/Models/Fornecedor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.CRUD.Models
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Nome de fornecedor não deve ser nulo!"), StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Nome de fornecedor não deve ser nulo!"), StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public int TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
